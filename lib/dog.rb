480b837b76374b85767aebb8754e8029c2904913
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-03 03:01:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Add your code here
+class Dog 
+@@all = Array.new #class variable set to an empty Array
+attr_accessor :name
+
+
+def initialized(name)
+  @name = name
+  @@all.push(self) 
+end
+
+def self.all
+  @@all
+end
+
+def self.print_all
+  @@all.each do |dog|
+    puts"#{dog}"
+  end
+end
+
+
+
+end
